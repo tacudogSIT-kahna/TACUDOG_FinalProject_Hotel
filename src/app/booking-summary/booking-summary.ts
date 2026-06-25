e4469209f66b502d73874e2c203d153ae841fea5
tacudogSIT-kahna <20187508@s.ubaguio.edu>
2026-06-25 09:40:49 +0800

    feat: initialize booking and UI components

---
+++ src/app/booking-summary/booking-summary.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-booking-summary',
+  imports: [],
+  templateUrl: './booking-summary.html',
+  styleUrl: './booking-summary.css',
+})
+export class BookingSummary {
+
+}
---
+++ src/app/room-card/room-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-room-card',
+  imports: [],
+  templateUrl: './room-card.html',
+  styleUrl: './room-card.css',
+})
+export class RoomCard {
+
+}
