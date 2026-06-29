a45540643e263c7f7e1611ad55da1f719315e6c5
tacudogSIT-kahna <20187508@s.ubaguio.edu>
2026-06-29 11:34:25 +0800

    feat: add manager-login and manager-dashboard components to workspace

---
+++ src/app/manager-dashboard/manager-dashboard.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manager-dashboard',
+  imports: [],
+  templateUrl: './manager-dashboard.html',
+  styleUrl: './manager-dashboard.css',
+})
+export class ManagerDashboard {}
---
+++ src/app/manager-login/manager-login.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manager-login',
+  imports: [],
+  templateUrl: './manager-login.html',
+  styleUrl: './manager-login.css',
+})
+export class ManagerLogin {}
